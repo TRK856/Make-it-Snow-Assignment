8b1401a6d36a9499d8092e76ecd819eec4e98804
Taha Kamil <t.kamil@share.epsb.ca>
2022-03-15 14:48:44 -0600

    slanted snow effect

--- main.js
+++ main.js
@@ -6,15 +6,24 @@ let ctx = cnv.getContext("2d");
 cnv.width = 800;
 cnv.height = 600;
 
+
+// let it snow assignment
+// define array
 let Snows = [];
 
-
+// automatically add snow
 setInterval(addSnow, 1000)
 
+// slanted snow effect
+let moveX = randomDec(-1,1)
+
+// event listener
 document.addEventListener("keydown", keydownHandler);
 
+// start drawing loop
 requestAnimationFrame(drawSnows);
 
+// litterally copied the bubbles array and made some changes and improvements
 function createRandomSnowArray(total){
     let temp = [];
     for(let n = 1; n <= total; n++){
@@ -34,25 +43,25 @@ function newSnow(initX, initY, initS, initO, initI, initColor){
     }
 }
 
+// changed the function tot he function i made in the draw functions assigmnet
 function drawSnow(aSnow){
     stroke(aSnow.color)
     fill(aSnow.color)
     drawAStar(aSnow.x, aSnow.y, aSnow.s, aSnow.o, aSnow.i, [true, true])
 }
 
-function moveSnow(aSnow, [moveX, moveY], interval, repeatedInterval){
+// changed the function so that x wouldn't be constantly being changed when drawsnows function is ran
+function moveSnow(aSnow, [moveX, moveY], intiX){
     if(moveX === true){
-        for(let i = 0; i <= repeatedInterval; i++){
-            aSnow.x += interval;
-            setTimeout(1000)
-        }
+        aSnow.x += intiX;
     } else if (moveY === true){
         aSnow.y += 1;
     }
 }
 
+// added a layer function, changing the size to add a sense of depth (nothing to crazy)
 function newRandomSnow(){
-    let layer = randomInt(-1, 4)
+    let layer = randomInt(0, 5)
     if(layer === 4){
         return{
             x:randomInt(-20, cnv.width),
@@ -61,15 +70,24 @@ function newRandomSnow(){
             o:randomInt(7,10),
             i:randomInt(3,5),
             color:"white"
-        }
-    }  else if (layer === 2 || layer === 3){
+        };
+    }  else if (layer === 3){
         return{
             x:randomInt(0, cnv.width),
             y:0,
             s:randomInt(6, 10),
             o:randomInt(4, 7),
             i:randomInt(1,3),
-            color:"white"
+            color:"#d3d3d3"
+        }
+    }  else if (layer === 2){
+        return{
+            x:randomInt(0, cnv.width),
+            y:0,
+            s:randomInt(6, 10),
+            o:randomInt(4, 7),
+            i:randomInt(1,3),
+            color:"#979291"
         }
     }  else {
         return{
@@ -78,7 +96,7 @@ function newRandomSnow(){
             s:randomInt(6, 10),
             o:randomInt(1,4),
             i:0,
-            color:"white"
+            color:"625A59"
         }
     }
 }
@@ -101,21 +119,19 @@ function drawSnows(){
 
     for(let i = 0; i < Snows.length; i++){
         moveSnow(Snows[i], [false, true]);
-        if (Snows[i].y > cnv.height){
-            Snows[i].y = 0;
+        if (Snows[i].y > cnv.height + 10){
+            Snows[i].y = -10;
+        }
+        moveSnow(Snows[i], [true, false], moveX)
+        if (Snows[i].x > cnv.width + 5){
+            Snows[i].x = -4;
+        } else if (Snows[i].x === -5){
+            Snows[i].x = cnv.width + 4;
         }
         drawSnow(Snows[i]);
     }
     requestAnimationFrame(drawSnows);
 }
-
-setInterval(() => {
-    for(let i = 0; i < Snows.length; i++){
-        moveSnow(Snows[i], [true, false], randomInt(-1,5), randomInt(0,10))
-        drawSnow(Snows[i]);
-        setTimeout(1000)
-    }
-}, 1000)
 
 function addSnow() {
     Snows.push(newRandomSnow());
