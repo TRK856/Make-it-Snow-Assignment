7ff762834ffdf3d5aca6122311c21075d812720b
Taha Kamil <t.kamil@share.epsb.ca>
2022-03-16 14:45:11 -0600

    finished the assigment

--- functions.js
+++ functions.js
@@ -250,3 +250,134 @@ function drawAnyShape(shapeCenterX, shapeCenterY, numberOfSides, size, [fill, st
 }
 
 // my functions (make it snow assignments)
+// litterally copied the bubbles array and made some changes and improvements
+function createRandomSnowArray(total){
+    let temp = [];
+    for(let n = 1; n <= total; n++){
+        temp.push(newRandomSnow());
+    }
+    return temp;
+}
+
+function newSnow(initX, initY, initS, initO, initI, initColor){
+    return{
+        x:initX,
+        y:initY,
+        s:initS,
+        o:initO,
+        i:initI,
+        color:initColor
+    }
+}
+
+// changed the function to the function i made in the draw functions assigmnet
+function drawSnow(aSnow){
+    stroke(aSnow.color)
+    fill(aSnow.color)
+    drawAStar(aSnow.x, aSnow.y, aSnow.s, aSnow.o, aSnow.i, [true, true])
+}
+
+// changed the function so that x wouldn't be constantly being changed when drawsnows function is ran
+function moveSnow(aSnow, [moveX, moveY], intiX, intiY){
+    if(moveX === true){
+        aSnow.x += intiX;
+    } if (moveY === true){
+        aSnow.y += intiY;
+    }
+}
+
+// added a layer function, changing the size to add a sense of depth (nothing to crazy)
+function newRandomSnow(){
+    let layer = randomInt(0, 5)
+    if(layer === 4){
+        return{
+            x:randomInt(-20, cnv.width),
+            y:0,
+            s:randomInt(6, 10),
+            o:randomInt(7,10),
+            i:randomInt(3,5),
+            color:"white"
+        };
+    }  else if (layer === 3){
+        return{
+            x:randomInt(0, cnv.width),
+            y:0,
+            s:randomInt(6, 10),
+            o:randomInt(4, 7),
+            i:randomInt(1,3),
+            color:"#d3d3d3"
+        }
+    }  else if (layer === 2){
+        return{
+            x:randomInt(0, cnv.width),
+            y:0,
+            s:randomInt(6, 10),
+            o:randomInt(4, 7),
+            i:randomInt(1,3),
+            color:"#979291"
+        }
+    }  else {
+        return{
+            x:randomInt(0, cnv.width),
+            y:0,
+            s:randomInt(6, 10),
+            o:randomInt(1,4),
+            i:0,
+            color:"625A59"
+        }
+    }
+}
+
+// keydown handler (Right Arrow = Add Snow) (Left Arrow = Remove Snow)
+function keydownHandler(event) {
+    if (event.keyCode === 39) {
+        addSnow()
+    } else if (event.keyCode === 37) {
+        Snows.pop();
+    }
+}
+
+// fraw the snow
+function drawSnows(){
+    // background color
+    background("black")
+    
+    randomNumber = randomInt(-1,1)
+    // Loop for each snow entity
+    for(let i = 0; i < Snows.length; i++){
+        // ensures varibles remain unchanged
+        const moveXtemp = moveX;
+        const moveYtemp = moveY
+
+        // random chance for a snowflake to move more on the x-axis -or-on the y-axis;
+        if(randomInt(0, 10) == 10){
+            moveXtemp = moveX++;
+        } else if (randomInt(0, 50) == 50) {
+            moveYtemp = moveY++;
+        }
+
+        // moves the snowflakes
+        moveSnow(Snows[i], [true, true], moveXtemp, moveYtemp);
+
+        // makes the snowflakes move infinitlyby creating horizontal and vertical boundries
+        if (Snows[i].y > cnv.height + 3){
+            Snows[i].y = -3;
+        } 
+        if (Snows[i].x > cnv.width + 3){
+            Snows[i].x = -2;
+        } 
+        if (Snows[i].x == -3){
+            Snows[i].x = cnv.width + 2;
+        }   
+
+        // draws the snowflake
+        drawSnow(Snows[i]);
+    }
+    // loops again
+    requestAnimationFrame(drawSnows);
+}
+
+// adds snow
+function addSnow() {
+    Snows.push(newRandomSnow());
+}
